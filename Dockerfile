64b0c819ebd651167291168bb9cc9113c116b568
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-07-13 14:03:08 +0300

    Add github actions

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ FROM vmware/photon:latest
 LABEL name="vCenter Appliance Simulator"
 LABEL description="A VMware vCenter API mock server based on govmomi"
 LABEL maintainer="satak"
+LABEL version="1.0.0"
 
 COPY --from=builder /go/bin/vcsim .
 CMD ["./vcsim", "-l", "0.0.0.0:443"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -51,10 +51,10 @@ resource "vsphere_virtual_machine" "vm" {
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
-  clone {
+  /*clone {
     template_uuid = data.vsphere_virtual_machine.template.id
 
-    /*
+
     customize {
       linux_options {
         host_name  = var.vm_name
@@ -65,8 +65,13 @@ resource "vsphere_virtual_machine" "vm" {
         ipv4_netmask = "24"
       }
     }
-    */
+
+  } */
+
+  clone {
+    template_uuid = "b487e561-5b48-535a-9ad8-b130c10eda5a"
   }
+
 
   wait_for_guest_net_routable = false
   wait_for_guest_ip_timeout   = 0
